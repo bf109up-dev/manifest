8c2b378c7fadbf5927d6581b7eb3cd85a7789557
Yang <yang@example.com>
2026-04-04 17:50:44 +0900

    Update initial qemuarm64 build enviroment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p output/qemuarm64/build
-KAS_BUILD_DIR=output/qemuarm64/build taskset -c 0-9 kas build kas/qemuarm64/kas-qemuarm64-tag.yml
+KAS_BUILD_DIR=output/qemuarm64/build kas build kas/qemuarm64/kas-qemuarm64.yml:kas/qemuarm64/kas-qemuarm64-lock.yml
